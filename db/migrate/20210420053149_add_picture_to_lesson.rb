a06d48a4f48469f169471537f65c3f7573c80066
Adriana Cabrera <adi_111@icloud.com>
2021-04-19 22:32:49 -0700

    settings for active storage

---
+++ db/migrate/20210420053149_add_picture_to_lesson.rb
@@ -0,0 +1,5 @@
+class AddPictureToLesson < ActiveRecord::Migration[6.1]
+  def change
+    add_column :lessons, :picture, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_234352) do
+ActiveRecord::Schema.define(version: 2021_04_20_053149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -28,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_234352) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
@@ -39,6 +68,8 @@ ActiveRecord::Schema.define(version: 2021_03_10_234352) do
     t.index ["lesson_id"], name: "index_questions_on_lesson_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "lessons", "courses"
   add_foreign_key "questions", "lessons"
 end
